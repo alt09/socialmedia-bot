50c9cc352cc7ea28f4740d87248c2053a21fe6c5
alt09 <113487690+alt09@users.noreply.github.com>
2024-07-06 21:36:28 -0700

    5 post on facebook (#7)
    * use the actual methods
    
    * post things in facebook using a the compleate xpath to open and send the message but uses the css selector when it tries to log in, all the password and name are fake it will not work

--- public/facebookBot.py
+++ public/facebookBot.py
@@ -1,15 +1,17 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 def load_facebook_info():
     """
     Function to load Facebook information from the 'facebook.txt' file and set them as environment variables.
     """
-    with open("facebook.txt", "r") as file:
+    facebook_info_path = r"C:\Users\Andres  Lara-trevino\programacion\projectos\python\socialmedia-bot\private\facebook.txt"#path of the file where you store your password and user 
+    with open(facebook_info_path, "r") as file:
         # Read each line from the file
         for line in file:
             # Split each line into key-value pair based on '=' separator
@@ -21,25 +23,28 @@ def get_latest_news():
     """
     Function to fetch the latest news from a news website and return it as a string.
     """
-    # Initialize WebDriver (ensure you have the correct driver executable path)
-    driver_path = "c:\\Program Files\\Google\\Chrome\\Application"  # Replace with the path to your WebDriver executable
-    driver = webdriver.Chrome(driver_path)
-    
-    try:
-        # Open the news website
-        news_url = "https://miroradio.com/"  # Replace with the news website URL
-        driver.get(news_url)
+    # Configure Chrome options to disable notifications
+   # chrome_options = webdriver.ChromeOptions()
+    #prefs = {"profile.default_content_setting_values.notifications": 2}
+    #chrome_options.add_experimental_option("prefs", prefs)
+
+    # Initialize WebDriver with the configured options
+    #driver = webdriver.Chrome(options=chrome_options)
+    #try:
+        ## Open the news website
+        #news_url = "https://miroradio.com/"  # Replace with the news website URL
+        #driver.get(news_url)
         
-        # Wait for the news element to be visible
-        wait = WebDriverWait(driver, 10)
-        news_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "css-selector-of-news-element")))  # Replace with the actual CSS selector of the news element
+        ## Wait for the news element to be visible
+        #wait = WebDriverWait(driver, 10)
+    news_element = "im searching for a programming mentor for the frc team 6413"#wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "css-selector-of-news-element")))  # Replace with the actual CSS selector of the news element
         
         # Extract the text from the news element
-        latest_news = news_element.text
+    latest_news = news_element
         
-    finally:
+    #finally:
         # Close the WebDriver
-        driver.quit()
+     #   driver.quit()
     return latest_news
 
 def main():
@@ -52,58 +57,57 @@ def main():
     # Get information from environment variables
     usr = os.getenv("USER")  # Retrieve Facebook username
     pwd = os.getenv("PASSWORD")  # Retrieve Facebook password
-    message = get_latest_news()  # Fetch the latest news to post
+    message = "im searching for a programming mentor for the frc team 6413"#wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "css-selector-of-news-element")))  # Replace with the actual CSS selector of the news element
+#get_latest_news()  # Fetch the latest news to post
     image_path = os.getenv("IMAGE_PATH")  # Retrieve path to the image to be posted
     group_links = os.getenv("GROUP_LINKS").split()  # Retrieve Facebook group links as a list
 
-    # Delete cache by setting preferences for the Firefox profile
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("browser.cache.disk.enable", False)
-    profile.set_preference("browser.cache.memory.enable", False)
-    profile.set_preference("browser.cache.offline.enable", False)
-    profile.set_preference("network.http.use-cache", False)
+    # Configure Chrome options to disable notifications
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument('--disable-cache')
 
-    # Initialize Firefox WebDriver with custom profile (ensure you have the correct driver executable path)
-    driver = webdriver.Firefox(executable_path=r'D:\Desktop\selenium\geckodriver', firefox_profile=profile)
-    driver.implicitly_wait(15)  # Set an implicit wait of 15 seconds
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(1)
+    
+    try:
+         # Login to Facebook
+        driver.get("https://www.facebook.com/login/")
+        sleep(1)
+        username_field = driver.find_element(By.CSS_SELECTOR, "#email")
+        username_field.send_keys(usr)
 
-    # Login to Facebook
-    driver.get("http://www.facebook.com")
-    sleep(3)
-    elem = driver.find_element_by_id("email")# TODO: research the current way to find this elements 
-    elem.send_keys(usr)  # Enter the Facebook username
-    elem = driver.find_element_by_id("pass")# TODO: research the current way to find this elements 
-    elem.send_keys(pwd)  # Enter the Facebook password
-    c = driver.find_element_by_id('loginbutton')# TODO: research the current way to find this elements 
-    c.click()  # Click the login button
-    sleep(3)
+        password_field = driver.find_element(By.CSS_SELECTOR, "#pass")
+        password_field.send_keys(pwd)
+        
+        login_button = driver.find_element(By.CSS_SELECTOR, "#loginbutton")
+        login_button.click()
+        sleep(3)
 
     # Loop through each Facebook group link to post the message and image
-    for group in group_links:
-        # Navigate to the Facebook group
-        driver.get(group)
-        sleep(5)  # Wait for the page to load
+        for group in group_links:
+            # Navigate to the Facebook group
+            driver.get(group)
+            sleep(3)  # Wait for the page to load
+            # Find the post box and enter the message
+            post_button = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/div[2]')            
+            post_button.click()
+            sleep(3)  # Wait for the message to be entered
 
-        # Find the post box and enter the message
-        post_box = driver.find_element_by_xpath("//*[@name='xhpc_message_text']")# TODO: research the current way to find this elements 
-        post_box.send_keys(message)
-        sleep(5)  # Wait for the message to be entered
+            message_box = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div[1]/p")
+            message_box.send_keys(message)
+            # If an image path is provided, upload the image
+            if image_path != "":
+                addMedia = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/div/div/div")
+                addMedia.send_keys(image_path)
 
-        # If an image path is provided, upload the image
-        if image_path != "":
-            addMedia = driver.find_element_by_xpath("//*[@data-testid='media-attachment-selector']")# TODO: research the current way to find this elements 
-            addMedia.click()
-            # Provide the image file path
-            driver.find_element_by_xpath("//*[@name='composer_photo']").send_keys(image_path)# TODO: research the current way to find this elements 
-
-        # Find the 'Post' button and click it
-        Post_button = driver.find_element_by_xpath("//*[@data-testid='react-composer-post-button']")# TODO: research the current way to find this elements 
-        sleep(5)  # Wait before clicking the post button
-        Post_button.click()
-        sleep(5)  # Wait for the post to be made
-
+            # Find the 'Post' button and click it
+            Post_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div")
+            Post_button.click()
+            sleep(10)  # Wait for the post to be made
+    finally:
     # Close the browser window
-    driver.close()
-
+        sleep(3)
 if __name__ == '__main__':
     main()
